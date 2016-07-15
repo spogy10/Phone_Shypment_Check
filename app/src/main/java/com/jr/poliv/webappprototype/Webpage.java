4cc78b04fabe5b54cbb2d8293986664e616a948f
Paul <poliverjr@hotmail.com>
2016-07-15 14:48:23 -0500

    - Displays webpage
    - Previous page on back button click
    
    - Zoom functions added  -  Zoom buttons hidden on sdk > 11

--- app/src/main/java/com/jr/poliv/webappprototype/Webpage.java
+++ app/src/main/java/com/jr/poliv/webappprototype/Webpage.java
@@ -1,29 +1,50 @@
 package com.jr.poliv.webappprototype;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Webpage extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webpage);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       WebView webView = (WebView) findViewById(R.id.webView);
+
+       webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("http://www.therecoveryunit.com/users/login");
+        webView.getSettings().setBuiltInZoomControls(true);
+        if (Build.VERSION.SDK_INT >= 11)
+            webView.getSettings().setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://therecoveryunit.com");//http://192.168.1.5/app/rcu/users/login
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
